36ea7e23f0bd0f56f510db2904681d427e9c87fa
Sakitha <sakithaudarashmika63@gmail.com>
2025-12-29 09:06:58 +0530

    feat: implement initial Express server setup with MongoDB connection and user routes

--- backend/server.js
+++ backend/server.js
@@ -13,6 +13,9 @@ app.use(cors());
 app.use(express.json());
 
 // Routes
+app.get('/', (req, res) => {
+    res.send('API is running...');
+});
 app.use('/api/users', userRoutes);
 
 // Database Connection
